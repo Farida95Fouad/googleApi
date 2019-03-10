f16f8b7e7ae722500be1d7a4e7cec74b3f9a4c22
Pavly Remon <pavlyremon@pavlys-mac.local>
2019-03-10 13:48:06 +0200

    Initial Commit

---
+++ googleApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  googleApi
+//
+//  Created by Pavly Remon on 3/10/19.
+//  Copyright © 2019 Pavly Remon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
